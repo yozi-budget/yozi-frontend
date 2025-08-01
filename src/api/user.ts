8c0c81f0027b7e23241450df854c6838c1fab23f
Jieun <jeeneep@naver.com>
2025-08-01 21:39:28 +0900

    user.ts 수정

--- src/api/user.ts
+++ src/api/user.ts
@@ -2,6 +2,6 @@
 import axios from '@/api/axios';
 
 export const getNickname = async (): Promise<string> => {
-  const response = await axios.get<{ nickname: string }>('/user/nickname');
+  const response = await axios.get<{ nickname: string }>('api/user/nickname');
   return response.data.nickname;
 };
