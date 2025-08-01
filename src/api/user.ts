e732480d0b11575b03850afd80a91f7b7bb4adfb
Jieun <jeeneep@naver.com>
2025-08-01 22:13:56 +0900

    user.ts 변경

--- src/api/user.ts
+++ src/api/user.ts
@@ -2,6 +2,6 @@
 import axios from '@/api/axios';
 
 export const getNickname = async (): Promise<string> => {
-  const response = await axios.get<{ nickname: string }>('api/user/nickname');
-  return response.data.nickname;
+  const response = await axios.get<string>('api/user/nickname');
+  return response.data;  // 바로 문자열 반환
 };
