2d4730b24ee819fa5995c6eb40641bc3158fd8d0
Jieun <jeeneep@naver.com>
2025-08-02 00:07:21 +0900

    api/categories 수정

--- src/api/categories.ts
+++ src/api/categories.ts
@@ -4,6 +4,6 @@ import api from "@/api/axios";
 import { Category } from "@/types/category";
 
 export const fetchCategories = async (): Promise<Category[]> => {
-  const response = await api.post<Category[]>("/api/categories");
+  const response = await api.get<Category[]>("/api/categories");
   return response.data;
 };
